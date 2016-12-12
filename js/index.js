9836c94f43e652d98b797662fb3d418a17e58636
1824420143@qq.com <1824420143@qq.com>
2016-12-12 17:06:26 +0800

    Signed-off-by: 1824420143@qq.com <1824420143@qq.com>

--- js/index.js
+++ js/index.js
@@ -5,9 +5,10 @@ let music_audio = document.getElementById('myAudio');
 //按钮添加点击事件
 let music_list = new Array('Are You Ok.mp3','爱的传说.mp3','男孩别哭.mp3','傻女.mp3','童话镇.mp3');//文件夹中音乐列表
 let music_number = 0;
-
+var span_interval,music_now_time_span;
 window.onload = function  () {
 	music_audio.src = './music/'+music_list[music_number];
+	span_interval = setInterval(function(){span_musictime_load(span_interval);},50);
 }
 
 play.onclick = play_click;//播放按钮的监听事件
@@ -17,9 +18,11 @@ function play_click(){
 	if(img_name == "play.png"){
 		music_audio.play();
 		play.src = './images/stop.png';
+		music_now_time_span = setInterval(function(){span_musicnowtime_load();},500);
 	}else{
 		music_audio.pause();
 		play.src = './images/play.png';
+		clearInterval(music_now_time_span);
 	}
 }
 
@@ -33,6 +36,7 @@ function next_click(){
 	}
 	music_audio.src = './music/'+music_list[music_number];
 	music_audio.play();
+	play.src = './images/stop.png';
 }
 
 last.onclick = last_click;//上一首按钮
@@ -45,4 +49,27 @@ function last_click(){
 	}
 	music_audio.src = './music/'+music_list[music_number];
 	music_audio.play();
+	play.src = './images/stop.png';
+}
+
+function span_musictime_load(span_interval) {
+	if(music_audio.readyState == 4){
+		let music_times = Math.floor(music_audio.duration);
+		let music_times_2 = Math.floor(music_times/60)+':'+music_times%60;
+
+		if(Math.floor(music_times/60)<10)　music_times_2 = '0'+music_times_2;
+
+		document.getElementById('music_length').innerHTML = music_times_2;
+		clearInterval(span_interval);
+	}
+}
+
+function span_musicnowtime_load(){
+		let music_now_time = Math.floor(music_audio.currentTime);
+		let music_now_time_1 = Math.floor(music_now_time/60);
+		let music_now_time_2 = Math.floor(music_now_time%60);
+		if(Math.floor(music_now_time_1/60)<10){music_now_time_1 = '0'+music_now_time_1;}
+		if(Math.floor(music_now_time_2%60)<10){music_now_time_2 = '0'+music_now_time_2;}
+		document.getElementById('music_surplus').innerHTML = music_now_time_1+':'+music_now_time_2;
+		console.log(music_now_time);
 }
